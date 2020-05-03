22dfaede61f3b26e21dfeaa39018f2a31f4ff889
Ken Alex Fassone <nexusventuri@gmail.com>
2020-05-04 00:08:23 +0100

    Add better descriptions, images and scripts
    Added some screenshots on the page, to help reader to understand how the
    tags are set up.
    Updated the gtm-config.json, by adding better triggers on reload and
    changing some colours in the UI.
    Added replace-gtm-tag.sh script to help the tag set up

---
+++ replace-gtm-tag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  echo "Please pass the tag as an argument e.g.:"
+  echo "$ $0 'GTM-XXXXXXX'"
+  exit 1
+fi
+
+sed -i "s/GTM-[^'\"]*\(['\"]\)/$1\1/g" index.html
